a1836c3b115d9ee7182d921e9c5dc71bf9d6eefa
nariman <mukhtarov.n@gmail.com>
2020-01-22 23:07:43 +0300

    Третье задание

---
+++ HW3.py
@@ -0,0 +1,21 @@
+#Вариант №1 ))))
+n = int(input('Введите число:'))
+n1 = n*11
+n2 = n*111
+
+result=n+n1+n2
+print(result)
+
+#Вариант2
+n = input('Введите число:')
+n1= n+n
+n2= n1+n
+n = int(n)
+n1 = int(n1)
+n2 = int(n2)
+result=n+n1+n2
+print(result)
+
+
+
+
