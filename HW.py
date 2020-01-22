650f5bea1fea2c44d1e547c12ba107537bdd9c2b
nariman <mukhtarov.n@gmail.com>
2020-01-22 23:06:43 +0300

    первое задание

---
+++ HW.py
@@ -0,0 +1,7 @@
+name = 'Нариман'
+age = 35
+print(f'Привет, меня зовут {name}, мне {age} лет')
+user_name = input('А как тебя зовут?:')
+user_age = input('Сколько тебе лет?:')
+
+print(f'Очень приятно {user_name}')
